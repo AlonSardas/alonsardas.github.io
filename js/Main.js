e241e657ebf658454782cf468c3f755823251fda
Alon Sardas <alonsardas@gmail.com>
2025-12-24 02:17:23 +0200

    Adding info panel

--- js/Main.js
+++ js/Main.js
@@ -1,8 +1,19 @@
 import * as THREE from 'three';
 import { UI } from './UIManager.js';
-// import { AppState } from './AppState.js';
 import { DesignMode } from './DesignMode.js';
 import { Interaction } from './Interaction.js';
+
+// ============================================
+// INFO PANEL TOGGLE
+// ============================================
+const infoBtn = document.getElementById('infoButton');
+const panel = document.getElementById('infoPanel');
+
+infoBtn.addEventListener('click', () => {
+    // panel.classList.add('active');
+    // infoBtn.style.visibility = 'hidden'; // Hide button when panel is open
+    panel.classList.toggle('active');
+});
 
 // ============================================
 // THREE.JS SCENE SETUP
@@ -22,14 +33,17 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('container').appendChild(renderer.domElement);
 
 // Lighting
-const ambientLight = new THREE.AmbientLight(0xffffff, 5.0);
+const ambientLight = new THREE.AmbientLight(0xffffff, 4.0);
 scene.add(ambientLight);
-const sunLight = new THREE.DirectionalLight(0xffffff, 3.5);
-sunLight.position.set(10, 20, 10);
-scene.add(sunLight);
-const fillLight = new THREE.PointLight(0xffffff, 2.8);
-fillLight.position.set(-10, -5, -10);
-scene.add(fillLight);
+const sunLight1 = new THREE.DirectionalLight(0xffffff, 3.5);
+sunLight1.position.set(-10, 20, 10);
+scene.add(sunLight1);
+// const sunLight2 = new THREE.DirectionalLight(0xffffff, 1.5);
+// sunLight2.position.set(10, -20, -10);
+// scene.add(sunLight2);
+// const fillLight = new THREE.PointLight(0xffffff, 2.8);
+// fillLight.position.set(-10, -5, -10);
+// scene.add(fillLight);
 
 // ============================================
 // INITIALIZATION & ANIMATION
@@ -41,11 +55,16 @@ function init() {
 
     Interaction.init(scene, camera, renderer);
 
+    renderer.domElement.addEventListener('mousedown', () => {
+        if (panel.classList.contains('active')) {
+            panel.classList.remove('active');
+        }
+    });
     document.addEventListener('mousedown', (e) => Interaction.onMouseDown(e));
     document.addEventListener('mousemove', (e) => Interaction.onMouseMove(e));
     document.addEventListener('mouseup', () => Interaction.onMouseUp());
     // document.addEventListener('wheel', (e) => Interaction.onWheel(e), { passive: false });
-    document.addEventListener('contextmenu', (e) => e.preventDefault()); // Disable right-click menu
+    // document.addEventListener('contextmenu', (e) => e.preventDefault()); // Disable right-click menu
 
     window.addEventListener('resize', () => {
         camera.aspect = window.innerWidth / window.innerHeight;
